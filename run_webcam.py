40d1dc67b2641b618330ff488bbc3ee43c1d7b03
lhmcerqueira <luizhenriquemc1@gmail.com>
2021-10-26 19:47:28 -0300

    preparing hand adjustments

--- run_webcam.py
+++ run_webcam.py
@@ -56,7 +56,7 @@ def lateral_elevation_phase_1(hand_to_hand_distance, shoulder_angle_l, shoulder_
         angle1 and angle5 are angle between neck,shoulder and wrist
         head_hand_dst_r and head_hand_dst_l are distance between head to hands.
     '''
-    if hand_to_hand_distance in range(80, 300) and shoulder_angle_l in range(100, 150) and shoulder_angle_r in range(100, 150) and head_hand_dst_r in range(100, 160) and head_hand_dst_l in range(100, 160):
+    if hand_to_hand_distance in range(80, 250) and shoulder_angle_l in range(100, 150) and shoulder_angle_r in range(100, 150) and head_hand_dst_r > 160 and head_hand_dst_l > 160 :
         return True
     return False
 def lateral_elevation_phase_2(hand_to_hand_distance, shoulder_angle_l, shoulder_angle_r, head_hand_dst_r, head_hand_dst_l):
@@ -80,9 +80,9 @@ def lateral_elevation_phase_3(hand_to_hand_distance, shoulder_angle_l, shoulder_
     if shoulder_angle_l in range(161, 190) and shoulder_angle_r in range(161, 190):
         return True
     return False
-def draw_str(dst, xxx_todo_changeme, s, color, scale):
+def draw_str(dst, coordinates, s, color, scale):
 
-    (x, y) = xxx_todo_changeme
+    (x, y) = coordinates
     if (color[0]+color[1]+color[2]==255*3):
         cv2.putText(dst, s, (x+1, y+1), cv2.FONT_HERSHEY_PLAIN, scale, (0, 0, 0), thickness = 4, lineType=10)
     else:
@@ -103,22 +103,25 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print("modo 0: Mapeamento apenas \nmodo 1: Elevação Lateral \nmode 2: prancha")
-    mode = int(input("Enter a mode : "))
+    mode = int(input("Selecione o modo : "))
 
     logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resize)
     if w > 0 and h > 0:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+        poseEstimator = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
     else:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
+        poseEstimator = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
     logger.debug('cam read+')
     cam = cv2.VideoCapture(args.camera)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 800);
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600);
     ret_val, image = cam.read()
     logger.info('cam image=%dx%d' % (image.shape[1], image.shape[0]))
     count = 0
     i = 0
     frm = 0
-    y1 = [0,0]
+    right_hand_prev = [0,0]
+    left_hand_prev = [0,0]
     global height,width
     orange_color = (0,140,255)
     green_color = (0,255,0)
@@ -129,7 +132,7 @@ if __name__ == '__main__':
     while True:
         ret_val, image = cam.read()
         i =1
-        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+        humans = poseEstimator.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
         pose = humans
         image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
         height,width = image.shape[0],image.shape[1]
@@ -146,6 +149,8 @@ if __name__ == '__main__':
                 elbow_angle_l =  angle_calc(find_point(pose, 5), find_point(pose, 6), find_point(pose, 7)) # angulo cptovelo esquerdo
                 elbow_angle_r =  angle_calc(find_point(pose, 2), find_point(pose, 3), find_point(pose, 4)) # angulo cptovelo direito
 
+                right_hand_point = find_point(pose, 7)   # ponto da mão direita atualmente
+                left_hand_point = find_point(pose, 4)
                 # logger.debug("*** Entrou aqui ***")
 
                 if (mode == 1) and action == 'Desconhecido':
@@ -165,9 +170,9 @@ if __name__ == '__main__':
                     if not (hand_to_hand_distance in range(70, 300)):
                         draw_str(image, (20, 50), "Continue elevando os bracos", red_color, 2)
                         # ver na deteccao de imagem sobre comparar y para subir
-                    elif not (elbow_angle_l in range(140, 170)):
+                    elif not (elbow_angle_l in range(130, 170)):
                         draw_str(image, (20, 50), "Cotovelo esquerdo fora do angulo adequado", red_color, 2)
-                    elif not (elbow_angle_r in range(140, 170)):
+                    elif not (elbow_angle_r in range(130, 170)):
                         draw_str(image, (20, 50), "Cotovelo direito fora do angulo adequado", red_color, 2)
                     else:
                         draw_str(image, (20, 50), "Eleve os bracos mantendo o angulo", green_color, 2)
@@ -175,12 +180,12 @@ if __name__ == '__main__':
                     action = "fase2"
                     #if prev_action == 'Unknown' or prev_action == "Unknown_First":
                     #    exercise_duration = time.time()
-                    draw_str(image, (20, 50), "Eleve os braços até ficarem paralelos ao solo", green_color, 2)
+                    draw_str(image, (20, 50), "Eleve os braços até ficarem \nparalelos ao solo", green_color, 2)
                 if (mode == 1) and action == "fase2":
                     # if m_pose in range(20,300) and angle1 in range(60,140) and angle5 in range(60,140) and head_hand_dst_r in range(100,145) and head_hand_dst_l in range(100,145):
-                    if not (elbow_angle_l in range(160, 190)):
+                    if not (elbow_angle_l in range(150, 190)):
                         draw_str(image, (20, 50), "Cotovelo esquerdo fora do angulo adequado", red_color, 2)
-                    elif not (elbow_angle_r in range(160, 190)):
+                    elif not (elbow_angle_r in range(150, 190)):
                         draw_str(image, (20, 50), "Cotovelo direito fora do angulo adequado", red_color, 2)
                     else:
                         draw_str(image, (20, 50), "Eleve os bracos ate ficarem paralelos ao solo", green_color, 2)
@@ -191,9 +196,9 @@ if __name__ == '__main__':
                     draw_str(image, (20, 50), "Desca os bracos mantendo o angulo correto", green_color, 2)
                 if (mode == 1) and action == "fase3":
                     # if m_pose in range(20,300) and angle1 in range(60,140) and angle5 in range(60,140) and head_hand_dst_r in range(100,145) and head_hand_dst_l in range(100,145):
-                    if not (elbow_angle_l in range(160, 190)):
+                    if not (elbow_angle_l in range(150, 190)):
                         draw_str(image, (20, 50), "Cotovelo esquerdo fora do angulo adequado", red_color, 2)
-                    elif not (elbow_angle_r in range(160, 190)):
+                    elif not (elbow_angle_r in range(150, 190)):
                         draw_str(image, (20, 50), "Cotovelo direito fora do angulo adequado", red_color, 2)
                     else:
                         draw_str(image, (20, 50), "Desca os bracos mantendo o angulo correto", green_color, 2)
@@ -226,14 +231,14 @@ if __name__ == '__main__':
                 angle6 =  angle_calc(find_point(pose,4), find_point(pose,3), find_point(pose,2))
                 angle8 =  angle_calc(find_point(pose,8), find_point(pose,9), find_point(pose,10))
 
-                if (mode == 4) and plank(angle2, angle6, angle4, angle8,head_hand_dst_r, head_hand_dst_l):
+                if (mode == 2) and plank(angle2, angle6, angle4, angle8,head_hand_dst_r, head_hand_dst_l):
                     action = "Plank"
                     #if prev_action == 'Unknown' or prev_action == "Unknown_First":
                     #    exercise_duration = time.time()
                     #logger.debug("*** Plank ***")
-                    draw_str(image, (20, 50), " Plank", orange_color, 2)
+                    draw_str(image, (20, 50), " Prancha", orange_color, 2)
                     logger.debug("*** Plank ***")
-                    cv2.putText(image,"PLANK", (20,50), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0,0,255), 2, 11)
+                    #cv2.putText(image,"PLANK", (20,50), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (0,0,255), 2, 11)
 
         cv2.putText(image,
                     "FPS: %f" % (1.0 / (time.time() - fps_time)),
